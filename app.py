522fdd9e8207c3302278ff948f2e8a0d408e6dc0
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-06 21:52:05 +0100

    add dao methode to get all users from db

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import database
+import users.dao
 
-from flask import Flask
-
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +9,15 @@ database.initialiseConnection()
 @app.route('/')
 def hello_world():  # put application's code here
   return 'Hello World!'
+
+@app.route('/util', methods=['GET'])
+def get_users():
+    try:
+
+        result = users.dao.getUsers()
+        return jsonify({'users': result}), 200
+    except (Exception) as e:
+        return jsonify({e.__class__.__name__: e.args[0]}), 500
 
 
 if __name__ == '__main__':
---
+++ users/dao.py
@@ -0,0 +1,32 @@
+import database
+import psycopg2
+import os
+
+def getUsers():
+    connection = database.initialiseConnection()
+    cursor = connection.cursor()
+    sql = "SELECT * FROM users"
+    resultsExportUsers = []
+    try:
+        cursor.execute(sql)
+        connection.commit()
+        results = cursor.fetchall()
+        for row in results:
+            user = {
+                "id_user": row[0],
+                "lastname": row[1],
+                "firstname": row[2],
+            }
+            resultsExportUsers.append(user)
+        return resultsExportUsers
+    except (Exception, psycopg2.DatabaseError) as e:
+        try:
+            print("SQL Error [%d]: %s" % (e.args[0], e.args[1]))
+            raise Exception from e
+        except IndexError:
+            print("SQL Error: %s" % str(e))
+            raise Exception from e
+    finally:
+        cursor.close()
+        connection.close()
+

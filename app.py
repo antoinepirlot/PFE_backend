ec021389e405ac4c5f73093ba98cdc1a1f3bc8fb
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-07 12:49:46 +0100

    ADD init coursesService, create Course model, init createRoute

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+import services.CoursesService as CoursesService
 
 app = Flask(__name__)
+courses_service = CoursesService.CoursesService()
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-  return 'Hello World!'
+    return 'Hello World!'
+
+
+@app.route("/createCourse", methods=["POST"])
+def create_course():
+    return courses_service.create_one_course(None)
 
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
---
+++ services/CoursesService.py
@@ -0,0 +1,7 @@
+class CoursesService:
+
+    def __init__(self):
+        pass
+
+    def create_one_course(self, course):
+        return

6f2abdda6e42a7162ea36f9a5a7baadf39e9f9cf
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-07 17:45:07 +0100

    change methode getUsers from UserDao to get all attributes from usrs table

--- donnees/UserDao.py
+++ donnees/UserDao.py
@@ -15,7 +15,12 @@ def getUsers():
             user = {
                 "id_user": row[0],
                 "lastname": row[1],
-                "firstname": row[2]
+                "firstname": row[2],
+                "email": row[3],
+                "pseudo": row[4],
+                "sexe": row[5],
+                "phone": row[6],
+                "password": row[7],
             }
             resultsExportUsers.append(user)
         return resultsExportUsers
@@ -27,6 +32,36 @@ def getUsers():
             print("SQL Error: %s" % str(e))
             raise Exception from e
     finally:
+        cursor.close()
+        connection.close()
+
+def getUser(id):
+    connection = database.initialiseConnection()
+    cursor = connection.cursor()
+    sql = "SELECT * FROM pfe.users WHERE id_user = %i" % (id)
+    try:
+        cursor.execute(sql)
+        connection.commit()
+        result = cursor.fetchone()
+        user = {
+            "id_user": result[0],
+            "email": result[1],
+            "last_name": result[2],
+            "first_name": result[3],
+            "password": result[4],
+            "campus": result[5],
+            "role": result[6]
+        }
+        return user
+    except (Exception, psycopg2.DatabaseError) as e:
+        try:
+            print("SQL Error [%d]: %s" % (e.args[0], e.args[1]))
+            raise Exception from e
+        except IndexError:
+            print("SQL Error: %s" % str(e))
+            raise Exception from e
+    finally:
+
         cursor.close()
         connection.close()
 

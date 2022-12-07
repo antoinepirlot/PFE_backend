8c3133eac97e26e3339c320161f71a88f75b101e
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-07 18:40:25 +0100

    ADD all the route createCourse

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import database
 import users.dao
-import services.CoursesService as CoursesService
+from services.CoursesService import CoursesService
+from models.Course import Course
 
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
+from flask import request
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
-courses_service = CoursesService.CoursesService()
+courses_service = CoursesService()
 database.initialiseConnection()
 
 
@@ -18,7 +21,20 @@ def hello_world():  # put application's code here
 
 @app.route("/createCourse", methods=["POST"])
 def create_course():
-    return courses_service.create_one_course(None)
+    # check body is not empty
+    if request.json['id_category'] is None or request.json['id_category'] < 1 or \
+            request.json['id_teacher'] is None or request.json['id_teacher'] < 1 or \
+            request.json['course_description'] is None or len(str(request.json['course_description']).strip()) == 0 or \
+            request.json['price_per_hour'] is None or request.json['price_per_hour'] <= 0 or \
+            request.json['city'] is None or len(str(request.json['city']).strip()) == 0 or \
+            request.json['country'] is None or len(str(request.json['country']).strip()) == 0 or \
+            request.json['id_level'] is None or request.json['id_level'] < 1:
+        return "Course is not in the good format", 400
+
+    new_course = Course(request.json['id_category'], request.json['id_teacher'], request.json['course_description'],
+                        request.json['price_per_hour'], request.json['city'], request.json['country'],
+                        request.json['id_level'])
+    return courses_service.create_one_course(new_course)
 
 
 @app.route('/util', methods=['GET'])

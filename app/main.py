36dbfc82e1b8f5549cd7d37af4df60bd706d8513
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-08 18:06:50 +0100

    Add: configuration to deploy on heroku

--- app/main.py
+++ app/main.py
@@ -10,6 +10,3 @@ cors = CORS(app)
 # Routes
 app.register_blueprint(courses.route, url_prefix="/courses")
 app.register_blueprint(users.route, url_prefix="/users")
-
-if __name__ == '__main__':
-    app.run()

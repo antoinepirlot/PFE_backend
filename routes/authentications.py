71e0eee6a718cdb5727e6e27b52396df83cec479
Stefan <stefan.mircovici@student.vinci.be>
2022-12-09 10:51:40 +0100

    [REF] add part of login route

--- routes/authentications.py
+++ routes/authentications.py
@@ -6,3 +6,17 @@ from services.UsersService import UsersService
 users_service = UsersService()
 
 route = Blueprint("authentications", __name__)
+
+# ########
+# ##POST##
+# ########
+@route.route("/login", methods=["POST"])
+def login():
+    # check body is not empty
+    if request.json['email'] is None or len(str(request.json['email']).strip()) == 0 or \
+            request.json['password'] is None or len(str(request.json['password']).strip()) == 0:
+        return "Login object is not in the good format", 400
+
+    return 1
+
+    #return courses_service.create_one_course(new_course).convert_to_json(), 201

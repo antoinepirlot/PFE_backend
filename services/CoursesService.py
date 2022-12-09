4c494f5d65aad353f39bed011954e45cc971c0db
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-09 15:57:03 +0100

    Refactor: move convert to json in service

--- services/CoursesService.py
+++ services/CoursesService.py
@@ -8,7 +8,10 @@ class CoursesService:
         pass
 
     def get_one(self, id_course):
-        return self.coursesDAO.get_one(id_course)
+        course = self.coursesDAO.get_one(id_course)
+        if course is not None:
+            return course.convert_to_json()
+        return None
 
     def create_one_course(self, course):
         return self.coursesDAO.createOneCourse(course)

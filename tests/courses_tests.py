3d3e0bc2e69c84a8f1064730405838b798012161
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-09 15:21:04 +0100

    Add: tests with mock

--- tests/courses_tests.py
+++ tests/courses_tests.py
@@ -1,12 +1,21 @@
 import unittest
-import requests
-from requests import Response
+from unittest.mock import Mock
+
+import routes.courses
+from data.CoursesDAO import CoursesDAO
+from data.services.DALService import DALService
 
 
 class CoursesTests(unittest.TestCase):
     def setUp(self):
-        self.base_url = "http://localhost:5000/courses"
-        self.course_1 = {
+        self.coursesDAO = CoursesDAO()
+
+        self.dal_service = DALService()
+        self.dal_service.start = Mock()
+        self.course_from_db = [(1, 1, "Cours permettant de vous introduire le langage PHP. Aucun prérequis "
+                                      "n'est nécessaire", 18.0, "Bruxelles", "Belgique", 1)]
+
+        self.course_json = {
             "city": "Bruxelles",
             "country": "Belgique",
             "course_description": "Cours permettant de vous introduire le langage PHP. Aucun prérequis "
@@ -18,18 +27,23 @@ class CoursesTests(unittest.TestCase):
         }
 
     def test_get_one_course_with_id_course_ok(self):
-        response = requests.get(f"{self.base_url}/1")
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(self.course_1, response.json())
+        self.dal_service.commit = Mock(return_value=self.course_from_db)
+        response = routes.courses.get_one(1)
+        self.assertEqual(self.course_json, response[1])
 
     def test_get_one_course_with_id_course_not_existing(self):
-        response = requests.get(f"{self.base_url}/8000")
-        # If the test return true, that means course with id 8000 exists
-        self.assertEqual(404, response.status_code)
+        self.dal_service.commit = Mock(return_value=[])
+        try:
+            routes.courses.get_one(100)
+        except Exception as e:
+            self.assertEqual(404, e.code)
 
     def test_get_one_course_with_id_course_lower_than_1(self):
-        response = requests.get(f"{self.base_url}/0")
-        self.assertEqual(400, response.status_code)
+        self.dal_service.commit = Mock(return_value=[])
+        try:
+            routes.courses.get_one(0)
+        except Exception as e:
+            self.assertEqual(400, e.code)
 
 
 if __name__ == '__main__':

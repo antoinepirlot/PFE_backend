e58b75327986d74163a4bb1a2f7bd3a5dc06eab5
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-11 15:20:52 +0100

    Refactor: tests to use app test client

--- tests/courses_tests.py
+++ tests/courses_tests.py
@@ -1,15 +1,15 @@
 import unittest
 from unittest.mock import Mock
 
-import routes.courses
+from app.main import app
 from data.CoursesDAO import CoursesDAO
 from data.services.DALService import DALService
 
 
 class CoursesTests(unittest.TestCase):
     def setUp(self):
+        self.app = app.test_client()
         self.coursesDAO = CoursesDAO()
-
         self.dal_service = DALService()
         self.dal_service.start = Mock()
         self.dal_service.commit_transaction = Mock()
@@ -42,42 +42,34 @@ class CoursesTests(unittest.TestCase):
 
     def test_get_one_course_with_id_course_ok(self):
         self.dal_service.execute = Mock(return_value=self.course_from_db)
-        response = routes.courses.get_one(1)
-        self.assertEqual(200, response[1])
-        self.assertEqual(self.course_json, response[0])
+        response = self.app.get("courses/1")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.course_json, response.get_json())
 
     def test_get_one_course_with_id_course_not_existing(self):
         self.dal_service.execute = Mock(return_value=[])
-        try:
-            routes.courses.get_one(100)
-        except Exception as e:
-            self.assertEqual(404, e.code)
+        response = self.app.get("courses/100")
+        self.assertEqual(404, response.status_code)
 
     def test_get_one_course_with_id_course_lower_than_1(self):
         self.dal_service.execute = Mock(return_value=[])
-        try:
-            routes.courses.get_one(0)
-        except Exception as e:
-            self.assertEqual(400, e.code)
+        response = self.app.get("courses/0")
+        self.assertEqual(400, response.status_code)
 
     def test_get_all_courses_from_teacher_id_ok(self):
         self.dal_service.execute = Mock(return_value=self.course_from_db_without_id_teacher)
-        response = routes.courses.get_all_courses_from_teacher(1)
-        self.assertEqual(200, response[1])
-        self.assertEqual([self.course_json_without_id_teacher], response[0])
+        response = self.app.get("courses/teacher/1")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual([self.course_json_without_id_teacher], response.get_json())
 
     def test_get_all_courses_from_teacher_id_lower_than_1(self):
-        try:
-            routes.courses.get_all_courses_from_teacher(0)
-        except Exception as e:
-            self.assertEqual(400, e.code)
+        response = self.app.get("courses/teacher/0")
+        self.assertEqual(400, response.status_code)
 
     def test_get_all_courses_from_teacher_id_not_existing(self):
         self.dal_service.execute = Mock(return_value=[])
-        try:
-            routes.courses.get_all_courses_from_teacher(100)
-        except Exception as e:
-            self.assertEqual(404, e.code)
+        response = self.app.get("courses/teacher/100")
+        self.assertEqual(404, response.status_code)
 
 
 if __name__ == '__main__':

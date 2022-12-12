cfe8dc73af6f9148a34c80566be875a3283716ac
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-13 00:25:28 +0100

    add test_create_rating_negative_id_teacher

---
+++ tests/ratings_tests.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import Mock
+
+from app.main import app
+from data.DAO.CategoriesDAO import CategoriesDAO
+from data.DAO.RatingsDAO import RatingsDAO
+from data.services.DALService import DALService
+
+
+class RatingsTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.ratings_dao = RatingsDAO()
+        self.dal_service = DALService()
+        self.dal_service.start = Mock()
+        self.dal_service.commit_transaction = Mock()
+        self.dal_service.rollback_transaction = Mock()
+        self.categories_from_db = [
+            (
+                1,
+                "Anglais"
+            ),
+            (
+                2,
+                "Math"
+            )
+        ]
+        self.categories_json = [{'id_category': 1, 'name': 'Anglais'}, {'id_category': 2, 'name': 'Math'}]
+        self.rating_json = {
+            "id_rater": 1,
+            "id_rated": -1,
+            "rating_text": "bof comme prof j'ai pas vrmt apprécié",
+            "rating_number": 3
+        }
+
+    def test_create_rating_negative_id_teacher(self):
+        self.dal_service.execute = Mock(return_value=self.categories_from_db)
+        response = self.app.post('/ratings/', data=self.rating_json)
+        self.assertEqual(400, response.status_code)
+
+
+if __name__ == '__main__':
+    unittest.main()

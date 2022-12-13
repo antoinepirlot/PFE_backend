1f11107291bfd1d4b42d3535dec5752ba4c0aec8
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-13 18:37:33 +0100

    refactor exceptions

--- models/Notification.py
+++ models/Notification.py
@@ -1,10 +1,20 @@
 class Notification:
-    def __init__(self, id_notification, id_user, notification_text, notification_date, seen):
+
+    def __init__(self, id_user, notification_text, id_notification=None, notification_date=None, seen=None):
         self.id_notification = id_notification
-        self.id_user = id_user
-        self.notification_text = notification_text
+        self._id_user = id_user
+        self._notification_text = notification_text
         self.notification_date = notification_date
         self.seen = seen
+
+    @property
+    def id_user(self):
+        return self._id_user
+
+    @property
+    def notification_text(self):
+        return self._notification_text
+
 
     def convert_to_json(self):
         return {"id_notification": self.id_notification,
--- routes/notifications.py
+++ routes/notifications.py
@@ -12,22 +12,16 @@ route = Blueprint("notification", __name__)
 # ###GET###
 # #########
 @route.route('/<int:id_user>', methods=['GET'])
-def get_notification_from_user(id_user):
-    try:
-        result = notification_service.get_notification_from_user(id_user)
-        users = []
-        for user in result:
-            users.append(user.convert_to_json())
-
-        return users, 200
-    except Exception as e:
-        return jsonify({e.__class__.__name__: e.args[0]}), 500
+def get_notifications_from_user(id_user):
+    result = notification_service.get_notifications_from_user(id_user)
+    notifications = []
+    for notification in result:
+        notifications.append(notification.convert_to_json())
+    return notifications, 200
 
 
 @route.route('', methods=['POST'])
 def add_notification():
-    try:
-        notification_service.add_notification(request.json)
-        return jsonify({'notification': 'notification created'}), 201
-    except Exception as e:
-        return jsonify({e.__class__.__name__: e.args[0]}), 500
+    notification = Notification(int(request.json['id_user']), str(request.json['notification_text']))
+    notification_service.add_notification(notification)
+    return jsonify({'notification': 'notification created'}), 201

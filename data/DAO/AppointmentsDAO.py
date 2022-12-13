0ce0f1074b22259ea7cd339dcd7cea7edeaae015
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-13 18:54:02 +0100

    refactor exceptions appointments

--- data/DAO/AppointmentsDAO.py
+++ data/DAO/AppointmentsDAO.py
@@ -13,81 +13,47 @@ class AppointmentsDAO:
               "FROM projet.appointments a, projet.courses c " \
               "WHERE a.id_course = c.id_course AND a.id_student = %(id_student)s AND c.id_teacher = %(id_teacher)s"
 
-        try:
-            results = self.dal.execute(sql, {"id_teacher": id_teacher, "id_student": id_student}, True)
-            if len(results) == 0:
-                return None
-            all_appointments = []
-            for row in results:
-                appointment = Appointment(int(row[0]), int(row[1]), str(row[2]), str(row[3]), str(row[4]), int(row[5]),
-                                          str(row[7]))
-                all_appointments.append(appointment)
-            return all_appointments
-        except (Exception, psycopg2.DatabaseError) as e:
-            try:
-                print("SQL Error [%d]: %s" % (e.args[0], e.args[1]))
-                raise e
-            except IndexError:
-                print("SQL Error: %s" % str(e))
-                raise e
+        results = self.dal.execute(sql, {"id_teacher": id_teacher, "id_student": id_student}, True)
+        if len(results) == 0:
+            return None
+        all_appointments = []
+        for row in results:
+            appointment = Appointment(int(row[0]), int(row[1]), str(row[2]), str(row[3]), str(row[4]), int(row[5]),
+                                      str(row[6]))
+            all_appointments.append(appointment)
+        return all_appointments
 
     def get_appointments_for_user(self, id_student):
         sql = "SELECT DISTINCT id_course, id_student, appointment_state, appointment_date, street, number_house, box_house " \
               "FROM projet.appointments " \
               "WHERE  id_student = %(id_student)s ORDER BY appointment_state DESC, appointment_date"
 
-        try:
-            results = self.dal.execute(sql, {"id_student": id_student}, True)
-            if len(results) == 0:
-                return None
-            all_appointments = []
-            for row in results:
-                appointment = Appointment(int(row[0]), int(row[1]), str(row[2]), str(row[3]), str(row[4]), int(row[5]),
-                                          str(row[6]))
-                all_appointments.append(appointment)
-            return all_appointments
-        except (Exception, psycopg2.DatabaseError) as e:
-            try:
-                print("SQL Error [%d]: %s" % (e.args[0], e.args[1]))
-                raise e
-            except IndexError:
-                print("SQL Error: %s" % str(e))
-                raise e
+        results = self.dal.execute(sql, {"id_student": id_student}, True)
+        if len(results) == 0:
+            return None
+        all_appointments = []
+        for row in results:
+            appointment = Appointment(int(row[0]), int(row[1]), str(row[2]), str(row[3]), str(row[4]), int(row[5]),
+                                      str(row[6]))
+            all_appointments.append(appointment)
+        return all_appointments
 
     def get_appointments_for_user_of_course(self, id_course, id_student):
         sql = "SELECT DISTINCT id_course, id_student, appointment_state, appointment_date, street, number_house, box_house " \
               "FROM projet.appointments " \
               "WHERE  id_course = %(id_course)s AND id_student = %(id_student)s"
 
-        try:
-
-            result = self.dal.execute(sql, {"id_course": id_course, "id_student": id_student}, True)
-            if len(result) == 0:
-                return None
-            result = result[0]
-            appointment = Appointment(int(result[0]), int(result[1]), str(result[2]), str(result[3]), str(result[4]),
-                                      int(result[5]), str(result[6]))
-            return appointment
-        except (Exception, psycopg2.DatabaseError) as e:
-            try:
-                print("SQL Error [%d]: %s" % (e.args[0], e.args[1]))
-                raise e
-            except IndexError:
-                print("SQL Error: %s" % str(e))
-                raise e
+        result = self.dal.execute(sql, {"id_course": id_course, "id_student": id_student}, True)
+        if len(result) == 0:
+            return None
+        result = result[0]
+        appointment = Appointment(int(result[0]), int(result[1]), str(result[2]), str(result[3]), str(result[4]),
+                                  int(result[5]), str(result[6]))
+        return appointment
 
     def update_appointment_state(self, id_course, id_student, appointment_state):
         sql = "UPDATE projet.appointments SET appointment_state = %(appointment_state)s " \
               "WHERE  id_course = %(id_course)s AND id_student = %(id_student)s"
 
-        try:
-
-            self.dal.execute(sql, {"id_course": int(id_course), "id_student": int(id_student), "appointment_state": str(appointment_state)})
-
-        except (Exception, psycopg2.DatabaseError) as e:
-            try:
-                print("SQL Error [%d]: %s" % (e.args[0], e.args[1]))
-                raise e
-            except IndexError:
-                print("SQL Error: %s" % str(e))
-                raise e
+        self.dal.execute(sql, {"id_course": int(id_course), "id_student": int(id_student),
+                               "appointment_state": str(appointment_state)})
--- routes/appointments.py
+++ routes/appointments.py
@@ -14,31 +14,22 @@ route = Blueprint("appointments", __name__)
 # #########
 @route.route('/<int:id_student>', methods=['GET'])
 def get_appointments(id_student):
-    try:
-        result = appointments_service.get_appointments_for_user(id_student)
-        appointments = []
-        for appointment in result:
-            appointments.append(appointment.convert_to_json())
-        return appointments, 200
-    except Exception as e:
-        return jsonify({e.__class__.__name__: e.args[0]}), 500
+    result = appointments_service.get_appointments_for_user(id_student)
+    appointments = []
+    for appointment in result:
+        appointments.append(appointment.convert_to_json())
+    return appointments
 
 
 @route.route('/<int:id_course>/<int:id_student>', methods=['GET'])
 def get_appointments_for_user_of_course(id_course, id_student):
-    try:
-        result = appointments_service.get_appointments_for_user_of_course(id_course, id_student)
+    result = appointments_service.get_appointments_for_user_of_course(id_course, id_student)
 
-        return result.convert_to_json(), 200
-    except Exception as e:
-        return jsonify({e.__class__.__name__: e.args[0]}), 500
+    return result.convert_to_json()
 
 
 @route.route('/<int:id_course>/<int:id_student>/state/<string:appointment_state>', methods=['PUT'])
 def update_appointments_state(id_course, id_student, appointment_state):
-    try:
-        appointments_service.update_appointments_state(id_course, id_student, appointment_state)
+    appointments_service.update_appointments_state(id_course, id_student, appointment_state)
 
-        return jsonify({"update done": ""}), 200
-    except Exception as e:
-        return jsonify({e.__class__.__name__: e.args[0]}), 500
+    return jsonify({"update done": ""})

b64da7d47dc595e8461d3051fdfd76d0a9a5aeae
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-14 13:22:33 +0100

    make usersservice fields private

--- services/UsersService.py
+++ services/UsersService.py
@@ -10,26 +10,24 @@ from data.services.DALService import DALService
 
 
 class UsersService:
-    users_dao = UsersDAO()
-    categories_dao = CategoriesDAO()
-    ratings_dao = RatingsDAO()
-    dal = DALService()
-
     def __init__(self):
-        pass
+        self._users_dao = UsersDAO()
+        self._categories_dao = CategoriesDAO()
+        self._ratings_dao = RatingsDAO()
+        self._dal = DALService()
 
     def get_users(self):
         """
         Get all users, from database.
         :return: the list of users
         """
-        self.dal.start()
+        self._dal.start()
         try:
-            users = self.users_dao.get_users()
-            self.dal.commit_transaction()
+            users = self._users_dao.get_users()
+            self._dal.commit_transaction()
             return users
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e
 
     def get_users_by_id(self, id):
@@ -38,40 +36,40 @@ class UsersService:
         :param: id_user: the user's id
         :return: the user, If there's no user, it returns None
         """
-        self.dal.start()
+        self._dal.start()
         try:
-            user = self.users_dao.get_user_by_id(id)
+            user = self._users_dao.get_user_by_id(id)
             if user is None:
                 abort(404, "User not found")
-            self.dal.commit_transaction()
+            self._dal.commit_transaction()
             return user
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e
 
     def get_teacher_by_id(self, id):
-        '''
+        """
         Get a user with its average rating and its categories
         :param id: id of the user
         :return: the user retrieved
-        '''
-        self.dal.start()
+        """
+        self._dal.start()
         try:
-            user = self.users_dao.get_user_by_id(id)
+            user = self._users_dao.get_user_by_id(id)
             if user is None:
                 raise NotFoundException("User not found")
-            all_skills = self.categories_dao.get_all_skills_categories(id)
+            all_skills = self._categories_dao.get_all_skills_categories(id)
             user.skills = all_skills
-            all_ratings = self.ratings_dao.get_ratings_from_teacher(id)
+            all_ratings = self._ratings_dao.get_ratings_from_teacher(id)
             if len(all_ratings) != 0:
                 all_ratings_counter = 0
                 for rating in all_ratings:
                     all_ratings_counter += rating.rating_number
                 user.average_rating = (all_ratings_counter / len(all_ratings))
-            self.dal.commit_transaction()
+            self._dal.commit_transaction()
             return user
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e
 
     def get_users_by_email(self, email):
@@ -80,15 +78,15 @@ class UsersService:
         :param: email: the user's email
         :return: the user, If there's no user, it returns None
         """
-        self.dal.start()
+        self._dal.start()
         try:
-            user = self.users_dao.get_user_by_email(email)
+            user = self._users_dao.get_user_by_email(email)
             if user is None:
                 raise NotFoundException("User not found")
-            self.dal.commit_transaction()
+            self._dal.commit_transaction()
             return user
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e
 
     def get_users_by_pseudo(self, pseudo):
@@ -97,15 +95,15 @@ class UsersService:
         :param: pseudo: the user's pseudo
         :return: the user, If there's no user, it returns None
         """
-        self.dal.start()
+        self._dal.start()
         try:
-            user = self.users_dao.get_user_by_pseudo(pseudo)
+            user = self._users_dao.get_user_by_pseudo(pseudo)
             if user is None:
                 raise NotFoundException("User not found")
-            self.dal.commit_transaction()
+            self._dal.commit_transaction()
             return user
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e
 
     def sing_in_user(self, user):
@@ -117,18 +115,18 @@ class UsersService:
         password = user['password']
         hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
         user['password'] = hashed.decode()
-        self.dal.start()
+        self._dal.start()
         try:
-            user_email = self.users_dao.get_user_by_email(user['email'])
+            user_email = self._users_dao.get_user_by_email(user['email'])
             if user_email is not None:
                 raise ConflictException("Cet email est déjà pris")
-            user_pseudo = self.users_dao.get_user_by_pseudo(user['pseudo'])
+            user_pseudo = self._users_dao.get_user_by_pseudo(user['pseudo'])
             if user_pseudo is not None:
                 raise ConflictException("Ce pseudo est déjà pris")
-            self.users_dao.sing_in_user(user)
-            self.dal.commit_transaction()
+            self._users_dao.sing_in_user(user)
+            self._dal.commit_transaction()
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e
 
     def logInUser(self, email, password):
@@ -152,13 +150,13 @@ class UsersService:
         :param: token: the user's token
         :return: the user, If there's no user, it returns None
         """
-        self.dal.start()
+        self._dal.start()
         try:
-            user = self.users_dao.get_user_by_id(token['id'])
+            user = self._users_dao.get_user_by_id(token['id'])
             if user is None:
                 raise NotFoundException("User not found")
-            self.dal.commit_transaction()
+            self._dal.commit_transaction()
             return user
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e

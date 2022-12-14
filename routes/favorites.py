1793098e2d9b5453b1c0731fa9de8ab6cddbea78
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-14 11:32:39 +0100

    Update: user can't add himself in his favorites

--- routes/favorites.py
+++ routes/favorites.py
@@ -49,6 +49,8 @@ def get_most_favorites_teachers():
 def add_favorite():
     new_favorite = Favorite.init_favorite_with_json(request.json)
     new_favorite.id_student = get_id_from_token(request.headers["authorization"])
+    if new_favorite.id_teacher is new_favorite.id_student:
+        raise BadRequestException("You can't add yourself in your favorites")
     return favorites_service.add_favorite(new_favorite).convert_to_json(), 201
 
 

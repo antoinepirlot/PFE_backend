69427739f656435128baf46f3b8bb0fa5b2f85c9
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-14 15:09:26 +0100

    make categories service a singleton

--- services/CategoriesService.py
+++ services/CategoriesService.py
@@ -3,28 +3,29 @@ from data.services.DALService import DALService
 
 
 class CategoriesService:
-    categories_dao = CategoriesDAO()
 
     def __init__(self):
-        self.dal = DALService()
+        pass
 
     def __new__(cls):
         if not hasattr(cls, "instance"):
             # No instance of CategoriesService class, a new one is created
+            cls._dal = DALService()
+            cls._categories_dao = CategoriesDAO()
             cls.instance = super(CategoriesService, cls).__new__(cls)
         # There's already an instance of CategoriesService class, so the existing one is returned
         return cls.instance
 
     def get_all_categories(self):
-        '''
+        """
         Get all categories
         :return: all categories
-        '''
-        self.dal.start()
+        """
         try:
-            all_categories = self.categories_dao.get_all_categories()
-            self.dal.commit_transaction()
+            self._dal.start()
+            all_categories = self._categories_dao.get_all_categories()
+            self._dal.commit_transaction()
             return all_categories
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal.rollback_transaction()
             raise e

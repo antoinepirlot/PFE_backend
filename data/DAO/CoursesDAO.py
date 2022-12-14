31df34e5739b743f1c1c1c39fd646bc5477a527e
Antoine Pirlot <pirlot.antoine@outlook.com>
2022-12-14 13:10:24 +0100

    Fix: remove getting color for getting all teacher's courses

--- data/DAO/CoursesDAO.py
+++ data/DAO/CoursesDAO.py
@@ -67,7 +67,7 @@ class CoursesDAO:
         sql = """
             SELECT
                    cou.id_course, cou.course_description, cou.price_per_hour, cou.city, cou.country, cou.level,
-                   cat.id_category, cat.name, cat.color,u.id_user, u.lastname, u.firstname, u.email, u.pseudo, u.sexe, u.phone,
+                   cat.id_category, cat.name,u.id_user, u.lastname, u.firstname, u.email, u.pseudo, u.sexe, u.phone,
                    COALESCE(SUM(ra.rating_number),0) AS "sum_stars", COUNT(ra.id_rated) AS "total_tuples_stars"
                 FROM 
                    projet.categories cat LEFT OUTER JOIN projet.courses cou ON cou.id_category = cat.id_category

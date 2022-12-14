2873aa480f122081ad2b3e30007b26a16aba24c0
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-12-14 15:22:44 +0100

    make notification and ratings service a singleton

--- services/RatingsService.py
+++ services/RatingsService.py
@@ -10,10 +10,18 @@ from data.services.DALService import DALService
 class RatingsService:
 
     def __init__(self):
-        self.ratings_DAO = RatingsDAO()
-        self.users_DAO = UsersDAO()
-        self.appointements_DAO = AppointmentsDAO()
-        self.dal = DALService()
+        pass
+
+    def __new__(cls):
+        if not hasattr(cls, "_instance"):
+            # No instance of RatingsService class, a new one is created
+            cls._appointments_DAO = AppointmentsDAO()
+            cls._ratings_DAO = RatingsDAO()
+            cls._users_DAO = UsersDAO()
+            cls._dal_service = DALService()
+            cls._instance = super(RatingsService, cls).__new__(cls)
+        # There's already an instance of RatingsService class, so the existing one is returned
+        return cls._instance
 
     def get_ratings(self, id_teacher):
         """
@@ -21,19 +29,19 @@ class RatingsService:
         :param: id_teacher: the user's id that gets a rating
         :return: list of ratings, If there's no ratings, it returns None
         """
-        self.dal.start()
+        self._dal_service.start()
         try:
-            user = self.users_DAO.get_user_by_id(id_teacher)
+            user = self._users_DAO.get_user_by_id(id_teacher)
             if user is None:
                 raise NotFoundException("User not found")
-            all_ratings = self.ratings_DAO.get_ratings_from_teacher(id_teacher)
+            all_ratings = self._ratings_DAO.get_ratings_from_teacher(id_teacher)
             for rating in all_ratings:
-                rater = self.users_DAO.get_user_by_id(rating.id_rater)
+                rater = self._users_DAO.get_user_by_id(rating.id_rater)
                 rating.setRater(rater)
-            self.dal.commit_transaction()
+            self._dal_service.commit_transaction()
             return all_ratings
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal_service.rollback_transaction()
             raise e
 
     def create_rating(self, rating):
@@ -42,30 +50,30 @@ class RatingsService:
         :param: rating: the rating to add
         :return: the rating added.
         """
-        self.dal.start()
         try:
-            if self.users_DAO.get_user_by_id(rating.id_rater) is None:
+            self._dal_service.start()
+            if self._users_DAO.get_user_by_id(rating.id_rater) is None:
                 raise NotFoundException("The student rater doesn't exist")
-            if self.users_DAO.get_user_by_id(rating.id_rated) is None:
+            if self._users_DAO.get_user_by_id(rating.id_rated) is None:
                 raise NotFoundException("The teacher rater doesn't exist")
             # check if a finish appointment exist
-            appointments = self.appointements_DAO.get_appointments_from_teacher_and_student(rating.id_rated,
+            appointments = self._appointments_DAO.get_appointments_from_teacher_and_student(rating.id_rated,
                                                                                             rating.id_rater)
             if appointments is None:
                 raise ForbiddenException("You have no course with this teacher")
-            isFinished = False
+            is_finished = False
             for appointment in appointments:
                 if appointment.appointment_state == 'finished':
-                    isFinished = True
-            if not isFinished:
+                    is_finished = True
+            if not is_finished:
                 raise ForbiddenException("You have not finished the course with this teacher")
             # check if a rating already exist
-            rating_db = self.ratings_DAO.get_rating_by_id_rater_and_id_rated(rating.id_rater, rating.id_rated)
+            rating_db = self._ratings_DAO.get_rating_by_id_rater_and_id_rated(rating.id_rater, rating.id_rated)
             if rating_db is not None:
                 raise ConflictException("You already give this teacher a rating")
-            rating = self.ratings_DAO.create_one_rating(rating)
+            rating = self._ratings_DAO.create_one_rating(rating)
+            self._dal_service.commit_transaction()
+            return rating
         except Exception as e:
-            self.dal.rollback_transaction()
+            self._dal_service.rollback_transaction()
             raise e
-        self.dal.commit_transaction()
-        return rating

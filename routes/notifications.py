ce4f35adedf01ef5d7a18e10839c1e1fe7db775c
Stefan <stefan.mircovici@student.vinci.be>
2022-12-15 17:29:26 +0100

    [REF] make route from notification service authorization

--- routes/notifications.py
+++ routes/notifications.py
@@ -4,6 +4,7 @@ from Exceptions.WebExceptions.BadRequestException import BadRequestException
 from services.NotificationsService import NotificationsService
 from models.Notification import Notification
 from utils.security import prevent_xss
+from utils.authorize import authorize
 
 notification_service = NotificationsService()
 
@@ -14,6 +15,7 @@ route = Blueprint("notification", __name__)
 # ###GET###
 # #########
 @route.route('/<int:id_user>', methods=['GET'])
+@authorize
 def get_notifications_from_user(id_user):
     result = notification_service.get_notifications_from_user(id_user)
     notifications = []
@@ -23,6 +25,7 @@ def get_notifications_from_user(id_user):
 
 
 @route.route('', methods=['POST'])
+@authorize
 def add_notification():
     json = prevent_xss(request.json)
     print(json['chat_link'])
@@ -36,6 +39,7 @@ def add_notification():
 
 
 @route.route('/update/<int:id_notification>', methods=['PUT'])
+@authorize
 def update_notification(id_notification):
     print(id_notification)
     notification_service.update_notification(id_notification)

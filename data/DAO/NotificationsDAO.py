0f061c1ee4d58ff9331d0cd1e82099297b5e3524
thomasbostaji <thomas.bostaji@student.vinci.be>
2022-12-15 13:12:52 +0100

    add methode to update notifications

--- data/DAO/NotificationsDAO.py
+++ data/DAO/NotificationsDAO.py
@@ -1,4 +1,5 @@
 import psycopg2
+from sqlalchemy import true, false
 
 from data.services.DALService import DALService
 from models.Notification import Notification
@@ -32,7 +33,7 @@ class NotificationsDAO:
         value = {"id_user": id_user}
         results = self._dal.execute(sql, value, True)
         for row in results:
-            notif = Notification(int(row[0]), str(row[1]), str(row[2]), int(row[3]), str(row[4]), bool(row[5]) )
+            notif = Notification(int(row[0]), str(row[1]), str(row[2]), int(row[3]), str(row[4]), bool(row[5]))
             results_export_notif.append(notif)
         return results_export_notif
 
@@ -50,6 +51,31 @@ class NotificationsDAO:
                             INSERT INTO projet.notifications VALUES (DEFAULT,%(id_user)s,%(text)s,now(),FALSE,%(chat_link)s) 
                         """
 
-            values = {"id_user": notification.id_user, "text": str(notification.notification_text), "chat_link": str(notification.chat_link)}
+            values = {"id_user": notification.id_user, "text": str(notification.notification_text),
+                      "chat_link": str(notification.chat_link)}
             self._dal.execute(sql, values)
 
+    def update_notification(self, id_notification):
+        """
+        Update a notification of a user
+        :param id_notification: notification to update
+        """
+        sql = """
+            UPDATE projet.notifications SET seen = true WHERE id_notification = %(id_notification)s
+                """
+
+        print(id_notification)
+        self._dal.execute(sql, {"id_notification": int(id_notification)})
+
+    def isNewNotification(self, id_user):
+        sql = """SELECT id_user, notification_text,chat_link, id_notification, notification_date, seen
+            FROM projet.notifications 
+            WHERE id_user = %(id_user)s AND seen = false;"""
+
+        results_export_notif = []
+        value = {"id_user": id_user}
+        results = self._dal.execute(sql, value, True)
+        for row in results:
+            notif = Notification(int(row[0]), str(row[1]), str(row[2]), int(row[3]), str(row[4]), bool(row[5]))
+            results_export_notif.append(notif)
+        return results_export_notif
